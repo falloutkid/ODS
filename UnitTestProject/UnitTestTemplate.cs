a7787a7d53b89a8c813fe44d988026abcd6f2e69
falloutkid <beck-13@mail.goo.ne.jp>
2018-01-05 07:32:45 +0900

    初期コミット

---
+++ UnitTestProject/UnitTestTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ODS;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestTemplate
+    {
+        public TestContext TestContext { set; get; }
+
+        [TestMethod]
+        [TestCase(10, 100)]
+        public void TestCaseMethod()
+        {
+            TestContext.Run((int input, int except) =>
+            {
+                Assert.AreEqual(except, Math.Pow(input, 2));
+            });
+        }
+    }
+}
