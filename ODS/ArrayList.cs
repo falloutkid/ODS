c75a206ede8cba784e8e8bd73b34d0de52c52c4e
falloutkid <beck-13@mail.goo.ne.jp>
2018-01-06 07:58:45 +0900

    2.2章のFastArrayStackまで完了

---
+++ ODS/ArrayList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODS
+{
+    class ArrayStack
+    {
+        void resize<T>(ref T[] a, int n)
+        {
+            T[] b = new T[n * 2];
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = a[i];
+            }
+            a = b;
+        }
+
+        void add<T>(ref T[] a, ref int n, int i, T x)
+        {
+            if (n + 1 > a.Length)
+                resize(ref a, n);
+            for (int j = n; j > i; j--)
+                a[j] = a[j - 1];
+            a[i] = x;
+            n++;
+        }
+
+        T remove<T>(ref T[] a, ref int n, int i)
+        {
+            T x = a[i];
+            for (int j = i; j < n - 1; j++)
+                a[j] = a[j + 1];
+            n--;
+            if (a.Length >= 3 * n)
+                resize(ref a, n);
+            return x;
+        }
+    }
+
+    class FastArrayStack
+    {
+        void resize<T>(ref T[] a, int n)
+        {
+            Array.Resize<T>(ref a, 2 * n);
+        }
+
+        void add<T>(ref T[] a, ref int n, int i, T x)
+        {
+            if (n + 1 > a.Length)
+                resize(ref a, n);
+
+            Array.Copy(a, i, a, i + 1, n - i);
+
+            a[i] = x;
+            n++;
+        }
+
+        T remove<T>(ref T[] a, ref int n, int i)
+        {
+            T x = a[i];
+            if (n + 1 > a.Length)
+                resize(ref a, n);
+
+            Array.Copy(a, i + 1, a, i, n - i - 1);
+            n--;
+
+            if (a.Length >= 3 * n)
+                resize<T>(ref a, n);
+
+            return x;
+        }
+    }
+}
--- ToDo List.md
+++ ToDo List.md
@@ -1,10 +1,95 @@
 # 第2章 配列を使ったリスト
 
 ## 2.1 ArrayStack：配列を使った高速なスタック操作
+
+通常の配列を扱った処理。実際の処理を行う際には次の2つを明確に区別する必要がある
+
+- a.Length：配列長(配列として確保しているメモリのサイズ)
+- n：格納しているデータのサイズ
+
+また、拡張と収縮を行う際には一定のルールの下、行う必要がある
+
+例えば、配列サイズを変更する関数(resize)を行うタイミングは以下のようにする
+
+- 拡張をする際にはメモリサイズが足りなくなった時
+- 収縮をする際には $a.Length \geqq 3n$ を満たす時
+
+また、resize関数は $a.Length = 2n$ にする関数とする
+
+| ArrayStackの動作 |
+|----|
+| ![](画像\ArrayStack.jpg) |
+
+```cs
+void resize<T>(ref T[] a, int n)
+{
+    T[] b = new T[n * 2];
+    for (int i = 0; i < n; i++)
+    {
+        b[i] = a[i];
+    }
+    a = b;
+}
+```
+
+この関数は既に関数として用意されている
+
+```cs
+Array.Resize<T>(ref a, 2 * n);
+```
+
 ## 2.2 FastArrayStack：最適化されたArrayStack
+
+ArrayStackの処理は3種類しかない
+
+- 使用するメモリのリサイズ
+- データのシフト
+	- Add関数とRemove関数
+- データのコピー
+	- メモリのリサイズをするときに実施
+
+データのシフトをfor文を使って行うことは非効率であり、既存の関数を使ったほうが良い
+
+```cs
+void resize<T>(ref T[] a, int n)
+{
+    Array.Resize<T>(ref a, 2 * n);
+}
+
+void add<T>(ref T[] a, ref int n, int i, T x)
+{
+    if (n + 1 > a.Length)
+        resize(ref a, n);
+
+    Array.Copy(a, i, a, i + 1, n - i);
+
+    a[i] = x;
+    n++;
+}
+
+T remove<T>(ref T[] a, ref int n, int i)
+{
+    T x = a[i];
+    if (n + 1 > a.Length)
+        resize(ref a, n);
+
+    Array.Copy(a, i + 1, a, i, n - i - 1);
+    n--;
+
+    if (a.Length >= 3 * n)
+        resize<T>(ref a, n);
+
+    return x;
+}
+```
+
+前記の手法と動作は変わらないが、コピー関数は最適化されているため、拘束に実行されることが多い。
+
+>将来的な最適化も実施されることが考えられるため、こちらを使用しておいたほうが良い
+
 ## 2.3 ArrayQueue：配列を使ったキュー
 ## 2.4 ArrayDeque：配列を使った高速な双方向キュー
-## 2.5 DualArrayDeque：2 つのスタックから作った双方向キュー
+## 2.5 DualArrayDeque：2つのスタックから作った双方向キュー
 ## 2.6 RootishArrayStack：空間効率に優れた配列スタック
 ## 2.7 ディスカッションと練習問題
 
